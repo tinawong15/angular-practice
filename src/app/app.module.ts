43f58eb2c599b3321fc05d3a10e847b3c894508f
tinawong15 <tinaw425@gmail.com>
2019-11-23 21:12:27 -0500

    added errors and route handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ToastrService } from './common/toastr.service'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
 import { CreateEventComponent } from './events/create-event.component'
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 
 @NgModule({
   imports: [
@@ -23,12 +25,22 @@ import { CreateEventComponent } from './events/create-event.component'
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if(component.isDirty) {
+    return window.confirm("You have not saved this event. Do you really want to cancel?")
+  }
+  return true
+}
