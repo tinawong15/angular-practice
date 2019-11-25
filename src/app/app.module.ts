6286df0b60e4a8853964ccab832ff2d3d29f6cdf
tinawong15 <tinaw425@gmail.com>
2019-11-25 14:02:18 -0500

    added index import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventsListResolver
+} from './events/index'
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventsListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
